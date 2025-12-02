fc2ec97c6d9c9930ce246f07e9db6a2f12265e8c
UCLCivSoc <su.civ-society@ucl.ac.uk>
2025-12-02 18:54:17 +0000

    Update config.js

--- competition/config.js
+++ competition/config.js
@@ -44,7 +44,18 @@ const CONFIG = {
     },
 
     // --- 5. FIREBASE ---
-    firebaseConfig: {
-        // apiKey: "YOUR_KEY_HERE"
-    }
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBnVixoZ0JYRV46EkDQa9XLumUfSmrfWVk",
+
+  authDomain: "contest-c8877.firebaseapp.com",
+
+  projectId: "contest-c8877",
+
+  storageBucket: "contest-c8877.firebasestorage.app",
+
+  messagingSenderId: "616984338580",
+
+  appId: "1:616984338580:web:c420d1d0594594b7e69481"
+
 };
